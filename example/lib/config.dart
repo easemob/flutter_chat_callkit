86f095d9c9134cfdd766498c3a62c52f7a836f5a
dujiepeng <347302029@qq.com>
2023-04-26 18:45:54 +0800

    update

--- example/lib/config.dart
+++ example/lib/config.dart
@@ -1,8 +1,12 @@
 class Config {
-  static String agoraAppId = "";
-  static String appkey = "";
+  static String agoraAppId = "15cb0d28b87b425ea613fc46f7c9f974";
+  static String appkey = "easemob#easeim";
 
-  static String appServerTokenURL = "";
-  static String appServerUserMapperURL = "";
-  static String appServerHost = "";
+  static String appServerTokenURL = "token/rtcToken/v1";
+  static String appServerUserMapperURL = "channel/mapper";
+  static String appServerHost = "a1.easemob.com";
+
+  static const String userId = "du001";
+  static const String password = "1";
+  static const String agoraToken = "";
 }
